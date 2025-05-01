1ea113b8ad700993ab39678af36d392b709f4231
Dmitry <156925436+DKhorov@users.noreply.github.com>
2025-05-01 21:40:46 +0300

    Update ad.jsx

--- src/apps/ad/ad.jsx
+++ src/apps/ad/ad.jsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 import img1 from '../../img/cs2.gif';
-import { FaReact, FaShoppingCart, FaUser } from 'react-icons/fa';
-import { GiCoffeeCup, GiShoppingBag } from 'react-icons/gi';
-import { IoMdSettings, IoIosNotifications } from 'react-icons/io';
-import { MdEmail, MdHome, MdStar } from 'react-icons/md';
-import { BsCalendar, BsChat } from 'react-icons/bs';
-import { AiOutlineSearch, AiOutlineHeart } from 'react-icons/ai';
-import { BiCamera, BiMusic } from 'react-icons/bi';
+import { FaWallet } from 'react-icons/fa';
 import '../../style/ad/ad.css';
 
-
 const Ad = () => {
+  const [balance, setBalance] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const userId = useSelector(state => state.auth.userId);
+  const token = useSelector(state => state.auth.token);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        if (!userId || !token) return;
+        
+        const response = await axios.get('/balance', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        
+        setBalance(response.data.balance || 0);
+        setLoading(false);
+      } catch (err) {
+        console.error('Ошибка при получении баланса:', err);
+        setError('Не удалось загрузить баланс');
+        setLoading(false);
+      }
+    };
+
+    fetchBalance();
+  }, [userId, token]);
+
   return (
-   <div className="ad">
-    <div className="adb1"><img src={img1} alt='' className='ad-logo'></img>
-    <h4 className='ad-title'>Wallet</h4></div>
-    <div className="adb2">
-    <h4 className='ad-title2'>--- Atom</h4>
+    <div className="ad">
+      <div className="adb1">
+        <img src={img1} alt='' className='ad-logo' />
+        <h4 className='ad-title'>Wallet</h4>
+      </div>
+      <div className="adb2">
+        <div className="balance-display">
+          <FaWallet className="wallet-icon" />
+          {loading ? (
+            <span className="balance-loading">Загрузка...</span>
+          ) : error ? (
+            <span className="balance-error">{error}</span>
+          ) : (
+            <span className="balance-amount">{balance.toFixed(2)} ₽</span>
+          )}
+        </div>
+        <h4 className='ad-title2'>--- Atom</h4>
+      </div>
     </div>
-
-
-
-   </div>
   );
 };
 
